42c75e5954e19d95cfcadcbbc4802882c5fb7da0
Cristina Shaver <cristina.shaver@gmail.com>
2017-05-13 21:55:12 -0700

    cleanup():

--- themes/delaunay/helper/index.js
+++ themes/delaunay/helper/index.js
@@ -1,20 +1,16 @@
-'use strict';
-
-// var log = require('hexo-log')();
-
 var moment = require('moment');
 
 var helpers = {
-  formatDate: function(date, format) {
-    if (date) {
-      return moment(date).format(format);
-    }
-  },
-  jsonStringify: function(object) {
-    return JSON.stringify(object, null, 2);
-  }
+	formatDate: function(date, format) {
+		if (date) {
+			return moment(date).format(format);
+		}
+	},
+	json: function(object) {
+		return JSON.stringify(object, null, 2);
+	}
 };
 
 module.exports = function(hexo) {
-  return helpers;
+	return helpers;
 };
